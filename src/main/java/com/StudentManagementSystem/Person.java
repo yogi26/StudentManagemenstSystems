5d5f5c2cd772ea24f7e6f9e61cafae43f7819969
Yogesh <yogeshgiri263@gmail.com>
2025-02-11 19:03:09 +0530

    initial commit

---
+++ src/main/java/com/StudentManagementSystem/Person.java
@@ -0,0 +1,93 @@
+package com.StudentManagementSystem;
+
+public class Person {
+	private static int totalPersonCount;
+	protected String name;
+	protected int age;
+	protected String address;
+	protected String phoneNumber;
+	public Person(String name, int age, String address, String phoneNumber) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		totalPersonCount++;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		if(validateAddress(address)) {
+			this.address = address;
+		}
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		if(validatePhoneNumber(phoneNumber)) {
+			this.phoneNumber=phoneNumber;
+		}
+	}
+	protected boolean validatePhoneNumber(String phoneNumber) {
+		if(phoneNumber.matches("\\d{10}")) {
+			return true;
+		}else {
+			System.err.println("Invalid phone number");
+			return false;
+		}
+	}
+	protected boolean validateAddress(String address) {
+		if(!address.isEmpty()||address==null) {
+			return true;
+		}else {
+			System.err.println("Invalid address");
+			return false;
+		}
+	}
+
+	public boolean updateInformation(String newAdderess, String newPhoneNumber) {
+		if(validateAddress(newAdderess)&&validatePhoneNumber(newPhoneNumber)) {
+			this.address=newAdderess;
+			this.phoneNumber=newPhoneNumber;
+			System.out.println("Updated address and contact number");
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean updateInformation(String newAdderess) {
+		if(validateAddress(newAdderess)) {
+			this.address=newAdderess;
+			System.out.println("Updated address");
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean updateInformation(String newPhoneNumber, boolean update) {
+		if(validatePhoneNumber(newPhoneNumber)) {
+			this.phoneNumber=newPhoneNumber;
+			System.out.println("Updated contact number");
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static int getTotoalPersonCount() {
+		return totalPersonCount;
+	}
+}
---
+++ src/main/java/com/StudentManagementSystem/Runner.java
@@ -0,0 +1,30 @@
+package com.StudentManagementSystem;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Person s1=new Student("Yogesh", 31, "Pune", "9988776655", 123, 90, 80, 85);
+		Person s2=new Student("Yogesh", 31, "Pune", "9988776655", 123, 90, 80, 85);
+		Person s3=new Student("Yogesh", 31, "Pune", "9988776655", 123, 90, 80, 85);
+		((Student)s1).calculateTotalMarks();
+		((Student)s1).calculatePercentage();
+		((Student)s1).calculateGrade();
+		((Student)s1).showStudentDetails();
+		
+		s1.updateInformation("7766552233");
+		((Student)s1).showStudentDetails();
+		
+		Person t1=new Teacher("Tejas", 33, "Pune", "8899776655", "1234", "Java programming", 3);
+		Person t2=new Teacher("Tejas", 33, "Pune", "8899776655", "1234", "Java programming", 3);
+		((Teacher)t1).calculateSalary();
+		((Teacher)t1).showTeacherDetails();
+		t1.updateInformation("Delhi");
+		((Teacher)t1).showTeacherDetails();
+		
+		System.out.println("Total students enrolled :"+Student.getTotalStudentsCount());
+		System.out.println("Total Teachers enrolled :"+Teacher.getTotalTeachersCount());
+		System.out.println("Total population in school :"+Person.getTotoalPersonCount());
+	}
+
+}
